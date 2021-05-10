b12ba105060fc71bd4fe90e8e20763dcf1492915
piccard <piccard@web.de>
2021-05-10 20:01:45 +0200

    php mongo running

--- config/database.php
+++ config/database.php
@@ -40,8 +40,8 @@ return [
             'host' => env('MONGO_DB_HOST', 'localhost'),
             'port' => env('MONGO_DB_PORT', 27017),
             'database' => env('MONGO_DB_DATABASE'),
-            'username' => env('MONGO_DB_USERNAME'),
-            'password' => env('MONGO_DB_PASSWORD'),
+            'username' => env('MONGO_DB_ROOT_USERNAME'),
+            'password' => env('MONGO_DB_ROOT_PASSWORD'),
             'options' => []
         ],
 
