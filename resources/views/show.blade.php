30e0fae257856df7e235dc25c0ed4564667728e1
piccard <piccard@web.de>
2021-05-12 17:19:48 +0200

    show blade

---
+++ resources/views/show.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col">
+                <div class="card">
+                    <div class="card-header">{{ __('Dashboard') }}</div>
+
+                    <div class="card-body">
+                        <table class="table">
+                            <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Method</th>
+                                <th scope="col">Type</th>
+                                <th scope="col">Message</th>
+                                <th scope="col">time</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach ($logs as $log)
+                                <tr>
+                                    <td>{{ $log["id"] }}</td>
+                                    <td>{{ $log["method"] }}</td>
+                                    <td class="@if($log["type"] === 'SUCCESS') table-success @else table-danger @endif">{{ $log["type"] }}</td>
+                                    <td>{{ $log["message"] }}</td>
+                                    <td>{{ $log["time"] }}</td>
+                                </tr>
+                            @endforeach
+
+                            </tbody>
+                        </table>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
