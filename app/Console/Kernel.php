bd069f6b59a884155026952d28351cc87b18e4eb
piccard <piccard@web.de>
2021-05-14 01:02:32 +0200

    schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,6 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
+        //$schedule->command('run-report')->weekly()->mondays()->at('12:00')->timezone('Pacific/Auckland');
         //$schedule->command('lakshmi:updateExchangeInfo')->hourlyAt(21);
 
         // $schedule->command('inspire')->hourly();
