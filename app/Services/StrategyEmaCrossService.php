a9c9e40815ece6aef958693b74cbde2a7b24e588
piccard <piccard@web.de>
2021-05-15 12:25:44 +0200

    emas calculating

--- app/Services/StrategyEmaCrossService.php
+++ app/Services/StrategyEmaCrossService.php
@@ -2,13 +2,10 @@
 
 namespace App\Services;
 
-use App\Models\JobLog;
 use App\Models\Symbol;
 use App\Models\Ema;
-use Carbon\Carbon;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\DB;
-use MongoDB\Client as MongoDB;
 
 class StrategyEmaCrossService {
 
@@ -57,7 +54,7 @@ class StrategyEmaCrossService {
         Log::info("Checking if trade can be triggered");
 
         if ($this->lakshmiService->job->next === "BUY") {
-            if ($this->currentEmas->value1 <= $this->currentEmas->value2) {
+            if ($this->currentEmas["ema1"] <= $this->currentEmas["ema2"]) {
                 Log::info("EMAs haven't crossed ... do nothing");
                 return false;
             } else {
@@ -65,7 +62,7 @@ class StrategyEmaCrossService {
                 return true;
             }
         } else {
-            if ($this->currentEmas->value1 >= $this->currentEmas->value2) {
+            if ($this->currentEmas["ema1"] >= $this->currentEmas["ema2"]) {
                 Log::info("EMAs haven't crossed ... do nothing");
                 return false;
             } else {
@@ -75,6 +72,10 @@ class StrategyEmaCrossService {
         }
     }
 
+    /**
+     * if job isn't active yet, this function tries to set the right status
+     * @return bool
+     */
     private function checkStatus() {
         Log::info("Job for " . $this->lakshmiService->job->symbol . " isn't ACTIVE");
         Log::info("Going to check if status can be changed");
@@ -86,7 +87,7 @@ class StrategyEmaCrossService {
             if ($this->lakshmiService->job->status === 'WAITING') {
                 Log::info("Job status for " . $this->lakshmiService->job->id . " is WAITING");
 
-                if ($this->currentEmas->value1 >= $this->currentEmas->value2) {
+                if ($this->currentEmas["ema1"] >= $this->currentEmas["ema2"]) {
                     $this->lakshmiService->job->status = "WAITING";
                 } else {
                     $this->lakshmiService->job->status = "READY";
@@ -97,7 +98,7 @@ class StrategyEmaCrossService {
             } else if ($this->lakshmiService->job->status === 'READY') {
                 Log::info("Job status for job " . $this->lakshmiService->job->id . " is READY");
 
-                if ($this->currentEmas->value1 >= $this->currentEmas->value2) {
+                if ($this->currentEmas["ema1"] >= $this->currentEmas["ema2"]) {
                     $this->lakshmiService->job->status = "ACTIVE";
 
                     $msg = "Job status for " . $this->lakshmiService->job->id . " set to ACTIVE";
@@ -112,7 +113,7 @@ class StrategyEmaCrossService {
             Log::info("Next job for $this->lakshmiService->job->id is BUY");
 
             if ($this->lakshmiService->job->status === 'WAITING') {
-                if ($this->currentEmas->value1 <= $this->currentEmas->value2) {
+                if ($this->currentEmas["ema1"] <= $this->currentEmas["ema2"]) {
                     $this->lakshmiService->job->status = "WAITING";
                 } else {
                     $this->lakshmiService->job->status = "READY";
@@ -121,7 +122,7 @@ class StrategyEmaCrossService {
                     $this->lakshmiService->log($msg, 'STRATEGY', 'INFO');
                 }
             } else if ($this->lakshmiService->job->status === 'READY') {
-                if ($this->currentEmas->value1 <= $this->currentEmas->value2) {
+                if ($this->currentEmas["ema1"] <= $this->currentEmas["ema2"]) {
                     $this->lakshmiService->job->status = "ACTIVE";
 
                     $msg = "job for " . $this->lakshmiService->job->symbol . " set to status ACTIVE";
@@ -137,6 +138,9 @@ class StrategyEmaCrossService {
         return $this->lakshmiService->job->status === 'ACTIVE';
     }
 
+    /**
+     * creates or gets the right mongo collection
+     */
     private function setEmaModel() {
         Log::info("Setting up EMA model");
 
@@ -150,20 +154,28 @@ class StrategyEmaCrossService {
         $this->emaModel = Ema::setCollection($collectionName);
     }
 
+    /**
+     * sets the current EMAs
+     */
     private function setCurrentEmas() {
         Log::info("Getting current EMAs ...");
 
-        $this->currentEmas = $this->emaModel->where([
-            "symbol" => $this->lakshmiService->job->symbol,
-            "timeframe" => $this->lakshmiService->job->timeframe,
-            "ema1" => $this->getEma1Setting(),
-            "ema2" => $this->getEma2Setting(),
-        ])
-            ->orderBy('open_time', 'desc')
-            ->first();
 
-        Log::info("Current EMA 1 is: " . $this->currentEmas->value1);
-        Log::info("Current EMA 2 is: " . $this->currentEmas->value2);
+        foreach (["ema1" => $this->getEma1Setting(), "ema2" => $this->getEma2Setting()] as $key => $emaRange) {
+            $currentEma= $this->emaModel->where([
+                "symbol" => $this->lakshmiService->job->symbol,
+                "timeframe" => $this->lakshmiService->job->timeframe,
+                "ema" => $emaRange,
+            ])
+                ->orderBy('open_time', 'desc')
+                ->first();
+            $this->currentEmas[$key] = $currentEma->value;
+
+        }
+
+
+        Log::info("Current EMA 1 is: " . $this->currentEmas["ema1"]);
+        Log::info("Current EMA 2 is: " . $this->currentEmas["ema2"]);
     }
 
     /**
@@ -269,75 +281,4 @@ class StrategyEmaCrossService {
     private function getEma2Setting() {
         return $this->lakshmiService->job->settings['ema2'];
     }
-
-    // ##############################################################
-
-    /**
-     * calculate the EMAs
-     * Until now from the very beginning
-     *
-     * @return array|array[]
-     * @throws \Exception
-     */
-    //private function calcEmas() {
-    //    Log::info('Start getting emas for ' . $this->lakshmiService->job->symbol . ' with timeframe ' .
-    //        $this->lakshmiService->job->timeframe);
-    //    Log::info('Ranges for EMAs are ' . $this->getEma1Setting() . ', ' . $this->getEma2Setting());
-    //
-    //    $symbolModel = Symbol::setCollection($this->lakshmiService->job->symbol);
-    //
-    //    // get close prices
-    //    $klines = $symbolModel->where([
-    //        "symbol" => $this->lakshmiService->job->symbol,
-    //        "timeframe" => $this->lakshmiService->job->timeframe
-    //    ])->orderBy('time', 'asc')->get();
-    //
-    //    // TODO ... MARCO!!!
-    //    // get rid of the last one and substitute it with the current price
-    //    $currentPrice = $this->lakshmiService->exchangeService->getCurrentPrice($this->lakshmiService->job->symbol);
-    //    $closePrices = $klines->pluck('close');
-    //    $closePrices->pop();
-    //    $closePrices->push($currentPrice["price"]);
-    //
-    //    // calc emas
-    //    $closePrices = $closePrices->toArray();
-    //    $emasRaw = [];
-    //    $emasRaw["ema1"] = trader_ema($closePrices, $this->getEma1Setting());
-    //    $emasRaw["ema2"] = trader_ema($closePrices, $this->getEma2Setting());
-    //
-    //    if (!is_array($emasRaw["ema1"]) || !is_array($emasRaw["ema2"])) {
-    //        throw new \Exception("trader_ema() didn't return an array");
-    //    }
-    //
-    //    $emas = [
-    //        "ema1" => [],
-    //        "ema2" => [],
-    //    ];
-    //    foreach ($emasRaw as $name => $emaRaw) {
-    //        foreach ($emaRaw as $key => $ema) {
-    //            $emas[$name][] = [
-    //                'time' => $klines->get($key)->time,    // TODO: open_time ... ist das richtig .. .denke schon?!?!
-    //                'value' => $ema
-    //            ];
-    //        }
-    //        Log::info('Finished getting ema for ' . $name);
-    //    }
-    //
-    //    return $emas;
-    //
-    //}
-
-    //private function createCollection(string $collectionName) {
-    //    $collectionExists = false;
-    //    foreach (DB::connection('mongodb')->listCollections() as $collectionInfo) {
-    //        if ($collectionInfo["name"] === $collectionName) {
-    //            $collectionExists = true;
-    //            break;
-    //        }
-    //    }
-    //
-    //    if (!$collectionExists) {
-    //        DB::connection('mongodb')->createCollection($collectionName);
-    //    }
-    //}
 }
